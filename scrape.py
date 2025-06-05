251dd540b7362cf8b2082bcba58a12e7e1df834b
gswirrl <guyjohnhilton@gmail.com>
2025-06-05 04:58:18 +0100

    refactor

--- scrape.py
+++ scrape.py
@@ -1,68 +1,65 @@
 import requests
-import pprint
 from bs4 import BeautifulSoup
 
-def value_of_element_id(soup, id):
-    element = soup.find(id=id)    
-    value = ""
-    if (element):
-        value = element['value']
-    return value
+def value_of_element_by_id(soup, element_id):
+    element = soup.find(id=element_id)
+    return element['value'] if element else ""
 
 def extract_params(soup):
-    return {
-        "__VIEWSTATE": value_of_element_id(soup, "__VIEWSTATE"),
-        "__EVENTTARGET":  value_of_element_id(soup, "__EVENTTARGET"),
-        "__EVENTARGUMENT":  value_of_element_id(soup, "__EVENTARGUMENT"),
-        "__VIEWSTATEGENERATOR":  value_of_element_id(soup, "__VIEWSTATEGENERATOR"),
-        "__PREVIOUSPAGE":  value_of_element_id(soup, "__PREVIOUSPAGE"),
-        "__EVENTVALIDATION":  value_of_element_id(soup, "__EVENTVALIDATION"),
-    }
+    param_ids = [
+        "__VIEWSTATE", "__EVENTTARGET", "__EVENTARGUMENT", 
+        "__VIEWSTATEGENERATOR", "__PREVIOUSPAGE", "__EVENTVALIDATION"
+    ]
+    return {param_id: value_of_element_by_id(soup, param_id) for param_id in param_ids}
 
-def get_home_page():
-    print(f"GET: {HOME_PAGE_URL}")
-    try:
-        response = requests.get(HOME_PAGE_URL)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return extract_params(soup)
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching home page: {e}")
+def fetch_page_params(url, method='GET', data=None):
+    response = fetch_page(url, method, data)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return extract_params(soup)
 
-def get_search_page(search_term,params):
-    params["ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl01$SearchText"]= search_term
-    params["ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl02$discoveryadvancce"]=""
-    print(f"POST: {SEARCH_PAGE_URL}")
-    try:
-        response = requests.post(SEARCH_PAGE_URL, data=params)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return extract_params(soup)
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching search page: {e}")
+    
+def fetch_page(url, method, data):
+   try:
+        print(f"{method}: {url}")
+        response = requests.request(method, url, data=data)
+        response.raise_for_status() 
+        return response
+   except requests.exceptions.RequestException as e:
+       print(f"Error fetching page ({url}): {e}")
+       return {}
 
-def get_extended_search_page(search_term,params):
-    params['__EVENTTARGET']="ctl00$main$TopPager$ctl15"
-    params["ctl00$main$TopPager$ctl15"]="100"
-    params["ctl00$main$BottomPager$ctl15"]="5"
-    params["ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl01$SearchText"]= search_term
-    print(f"POST: {SEARCH_PAGE_URL}")
-    try:
-        response = requests.post(SEARCH_PAGE_URL, data=params)
-        response.raise_for_status()
-        print(response.text)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return extract_params(soup)
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching extended page: {e}")
+def get_home_page_params():
+    return fetch_page_params(HOME_PAGE_URL)
+
+def get_search_page_params(search_term, params):
+    params.update({
+        "ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl01$SearchText": search_term,
+        "ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl02$discoveryadvancce": ""
+    })
+    return fetch_page_params(SEARCH_PAGE_URL, method='POST', data=params)
+
+def get_extended_search_page(search_term, params):
+    params.update({
+        '__EVENTTARGET': "ctl00$main$TopPager$ctl15",
+        "ctl00$main$TopPager$ctl15": "100",
+        "ctl00$main$BottomPager$ctl15": "5",
+        "ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl01$SearchText": search_term
+    })
+    return fetch_page(SEARCH_PAGE_URL, method='POST', data=params)
 
 def search(search_term):
-    current_params = get_home_page()
-    current_params=get_search_page(search_term,current_params)
-    current_params=get_extended_search_page(search_term,current_params)
+    params = get_home_page_params()
+    if not params:
+        return  
+    
+    params = get_search_page_params(search_term, params)
+    if not params:
+        return 
+    
+    result = get_extended_search_page(search_term, params)
+    print(result.text)
+    if not result:
+        return  
 
 HOME_PAGE_URL = 'https://archivesunlocked.northyorks.gov.uk/CalmView/default.aspx'
 SEARCH_PAGE_URL = "https://archivesunlocked.northyorks.gov.uk/CalmView/Overview.aspx"
