c4448af3fc67dafe8696020277d7174b97f5eebd
gswirrl <guyjohnhilton@gmail.com>
2025-06-05 13:23:32 +0100

    busted

--- scrape.py
+++ scrape.py
@@ -4,6 +4,13 @@ import cssutils
 from bs4 import BeautifulSoup
 from pprint import pprint
 
+session = requests.Session()
+
+def current_page_numnber(soup):
+    pager = soup.find(id="ctl00_main_TopPager")
+    current = pager.find(class_="Current")
+    return current.text if current else None
+
 def check_visibility(element):
     if not element:
         return False
@@ -36,8 +43,8 @@ def extract_params(soup):
 def fetch_page(url, method='GET', data=None):
     try:
         print(f"{method}: {url}")
-        response = requests.request(method, url, data=data)
-        response.raise_for_status() 
+        response = session.request(method, url, data=data)
+        response.raise_for_status()
         return response
     except requests.exceptions.RequestException as e:
         print(f"Error fetching page ({url}): {e}")
@@ -78,12 +85,36 @@ def parse_hits(soup):
     return [process_row(row) for row in rows if process_row(row)]
 
 def get_next_page(soup):
+    print(current_page_numnber(soup))
     pager = soup.find(id="ctl00_main_TopPager")
     next_wrapper = pager.find(class_="Next")
     if check_visibility(next_wrapper):
         anchor = next_wrapper.find("a")
-        return anchor["href"]
+        link = "https://archivesunlocked.northyorks.gov.uk" + anchor['href']
+        #print(link)
+        return link
     return False
+
+def fetch_next(search_term,next_page,params):
+    params.update({
+        "__EVENTTARGET": "ctl00$main$TopPager$ctl22",
+        "ctl00$main$TopPager$ctl15": "100",
+        "ctl00$main$BottomPager$ctl15": "100"
+    })
+    response = fetch_page("https://archivesunlocked.northyorks.gov.uk/CalmView/Overview.aspx", "POST", params)
+    if not response:
+        return {}
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    hits = parse_hits(soup)
+    next_page = get_next_page(soup)
+    params = extract_params(soup) if next_page else None
+
+    return {
+        "next_page": next_page,
+        "params": params,
+        "hits": hits
+    }
 
 def get_extended_search_page(search_term, params):
     params.update({
@@ -97,9 +128,6 @@ def get_extended_search_page(search_term, params):
         return {}
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    num_results = get_num_total_results(soup)
-    print(f"Total matches for search: {num_results}")
-
     hits = parse_hits(soup)
     next_page = get_next_page(soup)
     params = extract_params(soup) if next_page else None
@@ -123,17 +151,24 @@ def search(search_term):
     if not result:
         return
     
-    #pprint(result)
     resources, next_page, params = result ["hits"], result ["next_page"], result["params"]
+    
+    # if (next_page):
+    #     while True:
+    #         result = fetch_next(search_term,next_page,params)
+    #         resources.extend(result['hits'])
+    #         if result['next_page'] is False:
+    #             break
+    #         params = result['params']
+    #         next_page = result['next_page']
+    
     num_resources = len(resources)
-    
-    # TODO if there's a next page get it too and loop
-    
     print(f"Total relevant results: {num_resources}")
     pprint(resources)
 
 HOME_PAGE_URL = 'https://archivesunlocked.northyorks.gov.uk/CalmView/default.aspx'
 SEARCH_PAGE_URL = "https://archivesunlocked.northyorks.gov.uk/CalmView/Overview.aspx"
+
 
 if __name__ == "__main__":
     search("whitby stealing")
