3d01c0fa3cd538ce037e49de8a1263c765771032
gswirrl <guyjohnhilton@gmail.com>
2025-06-05 04:44:49 +0100

    wip

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import requests
+import pprint
 from bs4 import BeautifulSoup
 
 def value_of_element_id(soup, id):
@@ -18,29 +19,51 @@ def extract_params(soup):
         "__EVENTVALIDATION":  value_of_element_id(soup, "__EVENTVALIDATION"),
     }
 
-def get_homepage(url):
+def get_home_page():
+    print(f"GET: {HOME_PAGE_URL}")
     try:
-        response = requests.get(url)
+        response = requests.get(HOME_PAGE_URL)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
         return extract_params(soup)
         
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching initial page: {e}")
+        print(f"Error fetching home page: {e}")
 
-def preliminary_search(search_term,params,url):
+def get_search_page(search_term,params):
     params["ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl01$SearchText"]= search_term
     params["ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl02$discoveryadvancce"]=""
+    print(f"POST: {SEARCH_PAGE_URL}")
     try:
-        response = requests.post(url, data=params)
+        response = requests.post(SEARCH_PAGE_URL, data=params)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
-        print('Request was successful')
-        print(response.text) 
+        return extract_params(soup)
         
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching initial page: {e}")
+        print(f"Error fetching search page: {e}")
+
+def get_extended_search_page(search_term,params):
+    params['__EVENTTARGET']="ctl00$main$TopPager$ctl15"
+    params["ctl00$main$TopPager$ctl15"]="100"
+    params["ctl00$main$BottomPager$ctl15"]="5"
+    params["ctl00$search_DSCoverySearch1$ctl00_search_DSCoverySearch1_ctl01$SearchText"]= search_term
+    print(f"POST: {SEARCH_PAGE_URL}")
+    try:
+        response = requests.post(SEARCH_PAGE_URL, data=params)
+        response.raise_for_status()
+        print(response.text)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return extract_params(soup)
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching extended page: {e}")
     
+
+HOME_PAGE_URL = 'https://archivesunlocked.northyorks.gov.uk/CalmView/default.aspx'
+SEARCH_PAGE_URL = "https://archivesunlocked.northyorks.gov.uk/CalmView/Overview.aspx"
+
 if __name__ == "__main__":
-    params = get_homepage('https://archivesunlocked.northyorks.gov.uk/CalmView/default.aspx')
-    preliminary_search("whitby",params,"https://archivesunlocked.northyorks.gov.uk/CalmView/Overview.aspx")
+    current_params = get_home_page()
+    current_params=get_search_page("whitby",current_params)
+    current_params=get_extended_search_page("whitby",current_params)
