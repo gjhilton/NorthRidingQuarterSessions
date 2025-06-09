e815ac8d1e32f607b2548b6068a14cf9c38e1d38
G.J.Hilton <gjhilton@users.noreply.github.com>
2025-06-09 09:53:10 +0100

    wip

---
+++ summary_conviction_parser.py
@@ -0,0 +1,8 @@
+from summary_conviction_tests import run_all_tests, Case
+
+def parse(input_str: str) -> Case | None:
+    # This is a stub
+    return None
+
+if __name__ == "__main__":
+    run_all_tests(parse)
