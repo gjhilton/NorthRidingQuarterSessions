3aff9bb49253be4ba918baa75a87414ca1f0c309
G.J.Hilton <gjhilton@users.noreply.github.com>
2025-06-09 14:04:13 +0100

    j

--- summary_conviction_parser.py
+++ summary_conviction_parser.py
@@ -1,15 +1,23 @@
-import datetime
+import spacy
+import gender_guesser.detector as gender
+from pydantic import BaseModel
 from summary_conviction_testcases import Testcases, Case
 
-def parse(input_str: str) -> Case | None:
-    # dummy daya for now
-    result = {
-        "defendants": [
+nlp = spacy.load("en_core_web_sm")
+gender_detector = gender.Detector(case_sensitive=False)
+
+def extract_defendants(doc):
+    return ([
             {
                 "surname": "Waters",
                 "forenames": "William",
             }
-        ]
+        ])
+
+def parse(input_str: str) -> Case | None:
+    doc = nlp(input_str)
+    result = {
+        "defendants": extract_defendants(doc)
     }
     return Case(**result)
 
