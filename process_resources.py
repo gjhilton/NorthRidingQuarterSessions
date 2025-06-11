66d170cec15d3bbf97ab2adb81c62d31af8ecb75
gswirrl <guyjohnhilton@gmail.com>
2025-06-11 20:48:17 +0100

    more personal names

--- process_resources.py
+++ process_resources.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     #debug_parse_conviction_row(df, 2)
 
     #processed_df = process_dataframe(df,1,2)
-    processed_df = process_dataframe(df,2500,3000)
+    processed_df = process_dataframe(df,3000,3500)
     #processed_df = process_dataframe(df)
     processed_df = explode_defendants(processed_df)
     print(processed_df)
